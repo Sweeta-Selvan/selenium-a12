ae3517134720f49ef843df7d508a8906cfc62335
Sweeta-Selvan <sweetaselvan@gmail.com>
2025-08-15 13:46:55 +0530

    adding basics.py

--- training/basics.py
+++ training/basics.py
@@ -42,15 +42,17 @@
 # time.sleep(2)
 # driver.save_screenshot(r"C:\Users\Rajan Quvae\Desktop\selenium_training\training\screenshots\myn.png")
 # driver.close()
-# import time
-# from selenium import webdriver
-# opts=webdriver.ChromeOptions()
-# opts.add_experimental_option('detach',True)
-# driver=webdriver.Chrome(opts)
+import time
+from selenium import webdriver
+opts=webdriver.ChromeOptions()
+opts.add_experimental_option('detach',True)
+driver=webdriver.Chrome(opts)
 
-# driver.get('https://www.facebook.com/')
-# time.sleep(2)
-# driver.find_element('name','email').send_keys('sweet@gmail.com')
-# time.sleep(2)
-# driver.find_element('name','pass').send_keys('4321')
+class Registration:
+
+    driver.get('https://www.facebook.com/')
+    time.sleep(2)
+    driver.find_element('name','email').send_keys('sweet@gmail.com')
+    time.sleep(2)
+    driver.find_element('name','pass').send_keys('4321')
                                               
